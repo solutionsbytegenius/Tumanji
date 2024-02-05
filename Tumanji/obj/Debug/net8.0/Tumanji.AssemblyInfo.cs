9882fff4a755b27f7fc749e4702f752c4d6e63e6
Nicolò Di Marco <nicolas95ct@hotmail.it>
2024-02-05 23:09:02 +0100

    *

--- Tumanji/obj/Debug/net8.0/Tumanji.AssemblyInfo.cs
+++ Tumanji/obj/Debug/net8.0/Tumanji.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tumanji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269c17964360c4f1437bcf693461362cd9c11758")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tumanji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tumanji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
